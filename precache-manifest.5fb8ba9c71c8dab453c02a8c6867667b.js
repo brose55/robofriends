70140b8ec64d1fe55d885b7acc3486c594c1fbc3
Brandon Rose <brandonrose805@gmail.com>
2019-02-03 02:45:10 -0800

    Updates

---
+++ precache-manifest.5fb8ba9c71c8dab453c02a8c6867667b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "a72fa93d3cc5a90ab704",
+    "url": "/robofriends/static/js/main.a72fa93d.chunk.js"
+  },
+  {
+    "revision": "32c241efced8505c8b45",
+    "url": "/robofriends/static/js/1.32c241ef.chunk.js"
+  },
+  {
+    "revision": "a72fa93d3cc5a90ab704",
+    "url": "/robofriends/static/css/main.7a951e3a.chunk.css"
+  },
+  {
+    "revision": "32c241efced8505c8b45",
+    "url": "/robofriends/static/css/1.bfa264ed.chunk.css"
+  },
+  {
+    "revision": "d5e364c1c7824eae5a1968cae4c31076",
+    "url": "/robofriends/index.html"
+  }
+];
